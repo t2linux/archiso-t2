6d4c023c595b1fa8df8762b3a2992839ccc7d866
Noa Himesaka <himesaka@noa.codes>
2022-01-12 00:28:26 +0900

    automatically adds local repo

--- archiso/airootfs/root/runmefirst.sh
+++
@@ -1,4 +0,0 @@
-echo "This script will add local repository which contains necessary kernel to /etc/pacman.conf."
-echo "After running pacstrap and before you go into chroot, please, *please* copy /t2kern to /mnt/t2kern"
- 
-cat /root/t2kern.txt >> /etc/pacman.conf
