d2b9fb4af782228fe9b9f734954368ec81f861e7
Your Name <guidryheal@gmail.com>
2026-04-11 17:10:51 +0200

    fix docker

--- docker_builder.sh
+++ docker_builder.sh
@@ -1,3 +1,7 @@
  #/bin/bash
- # avoid to need arch to work on arch, also allow different type of virtualisation to build any to any
- docker run --rm -it --privileged   -v "${PWD}:/app" archlinux:latest  bash -lc 'pacman -Sy --needed --noconfirm wget archiso grub arch-install-scripts git base-devel && sudo grub-install /dev/sda && mkarchiso -v "/app/archiso"'
+
+ # so you don t need arch to build arch
+ # fixed tag also allow to fix version
+ # a .lock with version could be good as well
+ docker run --rm -it --privileged   -v "${PWD}:/app" archlinux:latest  bash -lc 'pacman -Sy --needed --noconfirm wget archiso grub arch-install-scripts git base-devel && mkarchiso -o "/app/out" -v "/app/archiso"'
+# https://docs.docker.com/engine/install/
